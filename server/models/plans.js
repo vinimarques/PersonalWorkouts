b0765a260a294335520f8090684587018c475164
Vinicius Marques <vinicius.marques@nodo.cc>
2017-10-11 19:05:44 -0300

    manager

--- server/models/plans.js
+++ server/models/plans.js
@@ -5,11 +5,14 @@ class Plans extends Model {
 
 	static all(company_id) {
 		return Model.query(`
-			SELECT p.id, p.name, p.days_per_week, p.difficulty_id, d.name as 'difficulty_name'
-			FROM plan as p
-			LEFT JOIN difficulty as d ON p.difficulty_id = d.id
-			LEFT JOIN plan_company as pc ON pc.plan_id = p.id
-			WHERE pc.company_id = ?
+			SELECT * FROM (
+				SELECT p.id, p.name, p.days_per_week, p.difficulty_id, d.name as 'difficulty_name', COUNT(day.id) as 'days'
+				FROM plan as p
+				LEFT JOIN difficulty as d ON p.difficulty_id = d.id
+				LEFT JOIN plan_company as pc ON pc.plan_id = p.id
+				LEFT JOIN day ON day.plan_id = p.id
+				WHERE pc.company_id = ?
+			) as result WHERE result.id > 0
 		`, [company_id]);
 	}
 

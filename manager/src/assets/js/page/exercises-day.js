b23ec6fbfea4c494488f10a7194f70c1658db840
Vinicius Roloff Marques <vinicius.roloffmarques@gmail.com>
2017-10-16 00:39:48 -0200

    manager

--- manager/src/assets/js/page/exercises-day.js
+++ manager/src/assets/js/page/exercises-day.js
@@ -5,6 +5,8 @@
  */
 
 import Page from '../components/page';
+import Validator from '../components/validator';
+import _ from 'lodash';
 
 /**
  *
@@ -12,10 +14,25 @@ import Page from '../components/page';
 class ExercisesDay extends Page {
 	constructor() {
 		super();
+		this.validator = new Validator();
 	}
 
 	init(page, ctx) {
 		super.load(page);
+		this.plan_id = parseInt(ctx.params.plan);
+		this.day_id = parseInt(ctx.params.day);
+		this.message = {
+			error: {
+				days: 'EXERCISES NOT FOUND'
+			},
+			success: {
+				add: 'Exercise has been adding successfully!',
+				update: 'Exercise has been updated successfully!',
+				remove: 'Exercise has been removed successfully!'
+			}
+		};
+
+		console.log(this);
 	}
 
 	_bindEvents() {

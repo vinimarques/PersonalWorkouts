8715f076e78a23c4703dfe1e0e63149b2a4e3b62
Vinicius Marques <vinicius.marques@nodo.cc>
2017-11-09 16:57:17 -0200

    manager

--- manager/src/assets/js/components/calendar.js
+++ manager/src/assets/js/components/calendar.js
@@ -2,6 +2,7 @@
 
 import Datepickk from '../libs/datepickk';
 import moment from 'moment';
+import _ from 'lodash';
 
 /**
  *
@@ -27,40 +28,86 @@ class Calendar {
 		});
 
 		this.exercises_days = exercises_days;
+		this.exercises_list = exercises_days;
+		this.exercises_select = [];
 		this.bindEvents();
 	}
 
 	bindEvents () {
-		this.calendar.onSelect = (e) => {
-			// console.log(e);
+		var that = this;
+
+		this.calendar.onSelect = function (checked) {
+			if (!checked && that.exercises_select.length > 0) {
+				// use moment to compare dates
+				let item = _.filter(that.exercises_select, (i) => { return moment(i.date, 'X').diff(this.toString()) === 0 })[0];
+				let list = _.filter(that.exercises_select, (i) => { return moment(i.date, 'X').diff(this.toString()) !== 0 });
+
+				that.exercises_list.push(item.exercise);
+				that.exercises_select = list;
+				that.renderDays();
+			}
 		};
 
 		$('body').on('click', '.d-table input', (ev) => {
 			let target = ev.target,
 				isSelected = target.checked,
-				label = $(target).next('label');
+				label = $(target).next('label'),
+				date = target.dataset.date;
 
 			if (isSelected) {
-				let position = label.position();
+				let position = label.offset();
 				let html = ['<div class="box-select-exercise">'];
+				let $tooltip, height;
 
-				this.exercises_days.map((exercise) => {
-					html.push(`<div class="box-select-exercise__item" data-id="${exercise.id}">${exercise.name}</div>`)
-				})
-
+				this.exercises_list.map((exercise) => {
+					html.push(`<div class="box-select-exercise__item" data-date="${date}" data-id="${exercise.id}">${exercise.name}</div>`)
+				});
 				html.push('</div>');
-				console.log(html)
+
+				$tooltip = $(html.join(''));
+				$('.box-select-exercise').remove();
+				$('body').append($tooltip);
+
+				height = $tooltip.height();
+				$tooltip.css({
+					top: position.top - height - 20,
+					left: position.left
+				})
 			}
-		})
+			else {
+				$('.box-select-exercise').remove();
+			}
+		});
+
+		$('body').on('click', '.box-select-exercise__item', (e) => {
+			if (this.calendar.locked) return false;
+
+			let id = parseInt(e.target.dataset.id),
+				date = e.target.dataset.date,
+				exercise = _.filter(this.exercises_list, (i) => {return i.id === id})[0];
+
+			this.exercises_select.push({
+				date: moment(date).format('X'),
+				exercise: exercise
+			});
+
+			this.exercises_list = _.filter(this.exercises_list, (i) => { return i.id !== id });
+			this.renderDays();
+			$('.box-select-exercise').remove();
+		});
 	}
 
 	renderDays () {
 		let html = [];
-		this.calendar.selectedDates.map((day, index) => {
+		let arr = this.exercises_select;
+		console.log(arr);
+		arr = _.sortBy(arr, ['date']);
+
+		arr.map((item) => {
 			html.push(`	<li>
-								<strong class="week">${moment(day).format('dddd')}:</strong>
-								<span class="exercise">${this.exercises_days[index].name}</span>
-								<strong class="day">${moment(day).format('DD/MM/YYYY')}</strong>
+								<strong class="week">${moment(item.date, 'X').format('dddd')}:</strong>
+								<span class="exercise">${item.exercise.name}</span>
+								<strong class="day">${moment(item.date, 'X').format('DD/MM/YYYY')}</strong>
 							</li>`);
 		});
 		this.daysWrapper.html(html.join(''));

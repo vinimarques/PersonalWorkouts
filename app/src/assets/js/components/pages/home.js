7f088b06e1ab820203c3f0f1964e7b2962a90d40
Vinicius Marques <vinicius.marques@nodo.cc>
2017-11-14 18:40:50 -0200

    app

--- app/src/assets/js/components/pages/home.js
+++ app/src/assets/js/components/pages/home.js
@@ -11,11 +11,25 @@ import Page from 'libs/page';
 class Home extends Page {
 
 	constructor (page) {
-		super(page);		
+		super(page);
 	}
 
 	onPageAfterAnimation () {
-		
+		this.bindEvents();
+	}
+
+	bindEvents () {
+		$('.page-home__content__exercises__item__title').on('click', function () {
+			$(this).parent().toggleClass('-active');
+		})
+
+		$('.btn-timer').on('click', function () {
+			$(this).toggleClass('-playing');
+		});
+
+		setTimeout(() => {
+			$('.-loading').removeClass('-loading');
+		}, 1000);
 	}
 }
 

e66605f6144a18646c2e418caad4e59ffa76e2f8
Vinicius Marques <vinicius.marques@nodo.cc>
2017-11-24 13:41:29 -0200

    provision

--- manager/src/assets/js/config.js
+++ manager/src/assets/js/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.version = '1.0.0';
-config.apiurl = 'api.personalworkouts.vm';
+config.apiurl = 'personal.v2rm.com.br/api';
 config.key = 'p3R$0n@L';
 
 config.timeCloseModal = 1200;
